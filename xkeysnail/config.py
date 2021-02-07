aef254d64c4c2fe123a16eb7079fb2044df603cf
yamdan <yamdan@gmail.com>
2021-02-07 09:33:11 +0900

    Change C-* to RC-*

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -50,18 +50,18 @@ define_conditional_multipurpose_modmap(lambda wm_class, device_name: device_name
 # Keybindings for Firefox/Chrome
 define_keymap(re.compile("Firefox|Google-chrome"), {
     # Ctrl+Alt+j/k to switch next/previous tab
-    K("C-M-j"): K("C-TAB"),
-    K("C-M-k"): K("C-Shift-TAB"),
+    K("RC-M-j"): K("C-TAB"),
+    K("RC-M-k"): K("C-Shift-TAB"),
     # Type C-j to focus to the content
-    K("C-j"): K("C-f6"),
+    K("RC-j"): K("C-f6"),
     # very naive "Edit in editor" feature (just an example)
-    K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
+    K("RC-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
 }, "Firefox and Chrome")
 
 # Keybindings for Zeal https://github.com/zealdocs/zeal/
 define_keymap(re.compile("Zeal"), {
     # Ctrl+s to focus search area
-    K("C-s"): K("C-k"),
+    K("RC-s"): K("C-k"),
 }, "Zeal")
 
 emacs_like_apps = (
@@ -72,64 +72,64 @@ emacs_like_apps = (
 # Emacs-like keybindings in non-Emacs applications
 define_keymap(lambda wm_class: wm_class not in emacs_like_apps, {
     # Cursor
-    K("C-b"): with_mark(K("left")),
-    K("C-f"): with_mark(K("right")),
-    K("C-p"): with_mark(K("up")),
-    K("C-n"): with_mark(K("down")),
-    K("C-h"): with_mark(K("backspace")),
+    K("RC-b"): with_mark(K("left")),
+    K("RC-f"): with_mark(K("right")),
+    K("RC-p"): with_mark(K("up")),
+    K("RC-n"): with_mark(K("down")),
+    K("RC-h"): with_mark(K("backspace")),
     # Forward/Backward word
     K("M-b"): with_mark(K("C-left")),
     K("M-f"): with_mark(K("C-right")),
     # Beginning/End of line
-    K("C-a"): with_mark(K("home")),
-    K("C-e"): with_mark(K("end")),
+    K("RC-a"): with_mark(K("home")),
+    K("RC-e"): with_mark(K("end")),
     # Page up/down
     K("M-v"): with_mark(K("page_up")),
-    K("C-v"): with_mark(K("page_down")),
+    K("RC-v"): with_mark(K("page_down")),
     # Beginning/End of file
     K("M-Shift-comma"): with_mark(K("C-home")),
     K("M-Shift-dot"): with_mark(K("C-end")),
     # Newline
-    K("C-m"): K("enter"),
-    K("C-j"): K("enter"),
-    K("C-o"): [K("enter"), K("left")],
+    K("RC-m"): K("enter"),
+    K("RC-j"): K("enter"),
+    K("RC-o"): [K("enter"), K("left")],
     # Copy
-    K("C-w"): [K("C-x"), set_mark(False)],
+    K("RC-w"): [K("C-x"), set_mark(False)],
     K("M-w"): [K("C-c"), set_mark(False)],
-    K("C-y"): [K("C-v"), set_mark(False)],
+    K("RC-y"): [K("C-v"), set_mark(False)],
     # Delete
-    K("C-d"): [K("delete"), set_mark(False)],
+    K("RC-d"): [K("delete"), set_mark(False)],
     K("M-d"): [K("C-delete"), set_mark(False)],
     # Kill line
-    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
+    K("RC-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
     # Undo
-    K("C-slash"): [K("C-z"), set_mark(False)],
-    K("C-Shift-ro"): K("C-z"),
+    K("RC-slash"): [K("C-z"), set_mark(False)],
+    K("RC-Shift-ro"): K("C-z"),
     # Mark
-    K("C-space"): set_mark(True),
-    K("C-M-space"): with_or_set_mark(K("C-right")),
+    K("RC-space"): set_mark(True),
+    K("RC-M-space"): with_or_set_mark(K("C-right")),
     # Search
-    K("C-s"): K("F3"),
-    K("C-r"): K("Shift-F3"),
+    K("RC-s"): K("F3"),
+    K("RC-r"): K("Shift-F3"),
     K("M-Shift-key_5"): K("C-h"),
     # Cancel
-    K("C-g"): [K("esc"), set_mark(False)],
+    K("RC-g"): [K("esc"), set_mark(False)],
     # Escape
-    K("C-q"): escape_next_key,
+    K("RC-q"): escape_next_key,
     # C-x YYY
-    K("C-x"): {
+    K("RC-x"): {
         # C-x h (select all)
         K("h"): [K("C-home"), K("C-a"), set_mark(True)],
         # C-x C-f (open)
-        K("C-f"): K("C-o"),
+        K("RC-f"): K("C-o"),
         # C-x C-s (save)
-        K("C-s"): K("C-s"),
+        K("RC-s"): K("C-s"),
         # C-x k (kill tab)
         K("k"): K("C-f4"),
         # C-x C-c (exit)
-        K("C-c"): K("C-q"),
+        K("RC-c"): K("C-q"),
         # cancel
-        K("C-g"): pass_through_key,
+        K("RC-g"): pass_through_key,
         # C-x u (undo)
         K("u"): [K("C-z"), set_mark(False)],
     }
