bb9a0ecd23a45a8d904acc7bcce14d2cf3ae8e3d
yamdan <yamdan@gmail.com>
2021-02-13 23:37:50 +0900

    Add keybindings for workspaces in GNOME esp. Pop Shell

---
+++ gnome/gsettings_keybinding_for_workspaces.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
+
+for i in {1..9}; do
+    gsettings set org.gnome.shell.keybindings switch-to-application-${i} []
+done
+
+for i in {1..9}; do
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-${i}  "['<Super>${i}']"
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-${i} "['<Super><Shift>${i}']"
+done
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10  "['<Super>0']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10  "['<Super><Shift>0']"
